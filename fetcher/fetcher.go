e67e65ef6a7f9d2a80196531384e61fa6ff83781
Daniel Purnomo <danielpurnomo100@gmail.com>
2026-04-21 13:09:30 +0200

    fix: enforce TLS 1.2 minimum version in IMAP and SMTP connections (#742)

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -264,6 +264,7 @@ func connectWithOptions(account *config.Account, extraOpts *imapclient.Options) 
 		TLSConfig: &tls.Config{
 			ServerName:         imapServer,
 			InsecureSkipVerify: account.Insecure,
+			MinVersion:         tls.VersionTLS12,
 		},
 	}
 	if extraOpts != nil {
